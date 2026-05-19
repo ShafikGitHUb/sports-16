e4bfcfc078ee9cc191e76bd5bd143b681cf6da45
ShafikGitHUb <princeshafik11@gmail.com>
2026-05-19 21:00:08 +0600

    add navbar and font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,22 @@
-import {Figtree,Geist,Geist_Mono } from "next/font/google";
+import {Figtree} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 const figtree = Figtree({
   subsets: ["latin"],
   variable: "--font-figtree",
     weight: ["400", "500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Sports-16",
